1c15d3447f83d0f6ae918d5e0dc6b6b168eb687d
Koudenatsu <7derpy@gmail.com>
2022-05-13 22:41:28 +0200

    Update rpi.py

--- rpi.py
+++ rpi.py
@@ -3,3 +3,11 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(22, GPIO.OUT)
 
 GPIO.output(22, 0)
+time.sleep(0.0000001)
+GPIO.output(22, 1)
+time.sleep(0.0000001)
+GPIO.output(22, 0)
+time.sleep(0.0000001)
+GPIO.output(22, 1)
+time.sleep(0.0000001)
+
