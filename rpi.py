7d4b2a132c0994f620af102105dc061c15f70f7a
Koudenatsu <7derpy@gmail.com>
2022-05-13 22:59:48 +0200

    Update rpi.py

--- rpi.py
+++ rpi.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+from itertools import repeat
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(22, GPIO.OUT)
 
-for i in xrange(10000):
+for i in repeat(None, 10000):
   GPIO.output(22, 1)
   time.sleep(0.0000001)
   GPIO.output(22, 0)
