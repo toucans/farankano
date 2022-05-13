435434755ccf698520185ec7e2d4cf59dd21218c
Koudenatsu <7derpy@gmail.com>
2022-05-13 22:57:00 +0200

    Update rpi.py

--- rpi.py
+++ rpi.py
@@ -3,13 +3,10 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(22, GPIO.OUT)
 
-GPIO.output(22, 0)
-time.sleep(0.0000001)
-GPIO.output(22, 1)
-time.sleep(0.0000001)
-GPIO.output(22, 0)
-time.sleep(0.0000001)
-GPIO.output(22, 1)
-time.sleep(0.0000001)
-GPIO.output(22, 0)
+for i in xrange(10000):
+  GPIO.output(22, 1)
+  time.sleep(0.0000001)
+  GPIO.output(22, 0)
+  time.sleep(0.0000001)
+
 
